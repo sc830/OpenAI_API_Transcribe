bb5257b2677d5eacd6ae46444ba4c9086dea0bc0
Ted Lehr <ted.lehr@txstate.edu>
2022-11-10 15:51:24 -0600

    Migrated changes from previous version/tag

--- routes.py
+++ routes.py
@@ -6,7 +6,8 @@ mail = Mail()
 
 app = Flask(__name__)
 
-app.secret_key = 'development key'
+app.config['SECRET_KEY']='LongAndRandomSecretKey'
+# app.secret_key = 'development key'
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
@@ -26,10 +27,14 @@ def about():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-  form = ContactForm()
+      # Flask 2.2.2 requires a parameter to a form object: request.form or other
+	  # See https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
+	  form = ContactForm(request.form)
 
   if request.method == 'POST':
     if form.validate() == False:
+      # This will print out any errors the form has to the user.  Used for debugging.
+      flash(form.errors)
       flash('All fields are required.')
       return render_template('contact.html', form=form)
     else:
