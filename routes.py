7b60ca50de5c468a02e6247b6a5a77b499083d68
Ted Lehr <ted.lehr@txstate.edu>
2022-11-12 13:38:24 -0600

    Added AppPassword and Update .gitignore

--- routes.py
+++ routes.py
@@ -4,19 +4,22 @@ from flask import Flask, flash, render_template, request
 from flask_mail import Mail, Message
 #forms is our local forms.py
 from forms import ContactForm
+import os
 
 app = Flask(__name__)
-mail = Mail(app)
 
 app.config['SECRET_KEY']='LongAndRandomSecretKey'
-# Commenting out the following to accept the defaults, see docs at:
-# https://pythonhosted.org/Flask-Mail/
-#app.config["MAIL_SERVER"] = "smtp.gmail.com"
-#app.config["MAIL_PORT"] = 465
-#app.config["MAIL_USE_SSL"] = True
-#app.config["MAIL_USERNAME"] = 'contact@example.com'
-#app.config["MAIL_PASSWORD"] = 'your-password'
-#mail.init_app(app)
+mail_user_name = os.getenv('GMAIL_USER_NAME')
+mail_app_password = os.getenv('GMAIL_APP_PASSWORD')
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = mail_user_name
+app.config['MAIL_PASSWORD'] = mail_app_password
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
 
 @app.route('/')
 def home():
@@ -38,7 +41,7 @@ def contact():
         flash('All fields are required.')
         return render_template('contact.html', form=form)
       else:
-        msg = Message(form.subject.data, sender='t_l81@txstate.edu', recipients=[form.email.data])
+        msg = Message(form.subject.data, sender=mail_user_name, recipients=['t_l81@txstate.edu'])
         msg.body = """
         From: %s <%s>
         %s %s
