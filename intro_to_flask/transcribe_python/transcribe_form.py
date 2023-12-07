e3c27decf9f34307d3231ca980e0818a758ec75c
Ted Lehr <ted.lehr@txstate.edu>
2023-12-07 07:59:57 -0600

    Refactors so it runs but doesn't have transcription

--- intro_to_flask/transcribe_python/transcribe_form.py
+++ intro_to_flask/transcribe_python/transcribe_form.py
@@ -7,4 +7,6 @@ from flask_wtf import Form
 from wtforms import TextAreaField, SubmitField, validators, ValidationError
 
 class TranscribemeForm(Form):
- #You create the form
+    #You create the form
+    prompt = "you change this"
+    submit = "you change this"
